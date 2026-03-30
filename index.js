bb2c926c6d95201cf4903c627ec2ac200fde812c
krish-patel-simform <krish.patel@simformsolutions.com>
2026-03-30 10:08:21 +0530

    feat:implement basic operations

--- index.js
+++ index.js
@@ -1,1 +1,137 @@
+import {infixEvalution} from './stack.js'
+ 
 // Dom Manipulations
+const keyPad = document.querySelector('.calculator-operation__buttons')
+ 
+const screen = document.getElementById('screen')
+console.log(screen)
+ 
+ 
+//* attack and onClick event
+ 
+keyPad.addEventListener('click',(e)=>{
+ 
+    const value = e.target.dataset.value;
+   
+    switch(value)
+    {
+        case "equals":
+            equals();
+            break;
+        case "clear":
+            clearScreen()
+            break;  
+        case "erase":
+            eraseLast();
+            break;
+        case "factorial":
+            screen.value = screen.value.slice(0,-1) +  +factorial(screen.value.at(-1))
+            break;
+        case 'sqrt':
+            sqrt();
+            break;
+        case 'log':
+            log();
+            break;
+        case 'square':
+            square();
+            break;
+        case 'powerOf10':
+            powerOf10();
+            break;
+        default:
+            screen.value += value;
+    }
+    console.log(value);
+ 
+    // call the infix Evalurion
+ 
+    e.stopPropagation();
+},true)
+ 
+//^ utility function
+ 
+function getNumberForOperation()
+{
+    let ind = screen.value.length - 1;
+    let value = '';
+    console.log("called")
+    // until we did not get non digit
+    while(ind >= 0 && Number(screen.value.at(ind))!== NaN)
+    {
+        // console.log("before",screen.value)
+        value = screen.value.at(-1) + value;
+        // console.log("after",screen.value)
+        screen.value = screen.value.slice(0,-1);
+        // console.log("VAlue:",value)
+        ind--;
+    }
+    return value
+}
+ 
+ 
+function equals()
+{
+    const result = infixEvalution(screen.value);
+    console.log("result:" + result)
+    screen.value = result;
+}
+ 
+function clearScreen()
+{
+    screen.value = ""
+}
+ 
+function eraseLast()
+{
+    screen.value = screen.value.slice(0,-1)
+}
+ 
+function factorial(n)
+{
+    let fact = 1;
+    for(let i=1;i<=n;i++)
+    {
+        fact *= i
+    }
+    console.log("fact:s" + fact)
+    return fact;
+}
+ 
+function sqrt()
+{
+    // let ind = screen.value.length - 1;
+    // let value = '';
+    // console.log("called")
+    // // until we did not get non digit
+    // while(ind >= 0 && Number(screen.value.at(ind)))
+    // {
+    //     // console.log("before",screen.value)
+    //     value = screen.value.at(-1) + value;
+    //     // console.log("after",screen.value)
+    //     screen.value = screen.value.slice(0,-1);
+    //     // console.log("VAlue:",value)
+    //     ind--;
+    // }
+    // find the value
+    const result = getNumberForOperation();
+    screen.value += Math.sqrt(result)
+}
+ 
+function square()
+{
+    const result = getNumberForOperation()
+    screen.value += Math.pow(result,2);
+}
+ 
+function log()
+{
+    const result = getNumberForOperation();
+    screen.value += Math.log10(result)
+}
+ 
+function powerOf10()
+{
+    const result = getNumberForOperation();
+    screen.value += Math.pow(10,result);
+}
--- stack.js
+++ stack.js
@@ -1,25 +1,187 @@
-class Stack{
-
-    push()
-    {
-
-    }
-    
-    pop()
-    {
-
-    }
-    
-    top()
-    {
-
-    }
-}
-
-//* infix -> postfix-> eveluate 
-
-
-function infixToPostFix(expression)
+Array.prototype.isEmpty = function()
 {
-
+    return this.length === 0;
 }
+Array.prototype.top = function()
+{
+    return this[this.length-1];
+}
+ 
+// const stack = []
+let operandStack;
+let operatorStack;  
+ 
+const precidence = {
+    '+' : 1,
+    '-' : 1,
+    '*' : 2,
+    '/' : 2,
+    '*' : 2,
+    '%' : 2,
+    '^' : 3,
+}
+ 
+ 
+function isOperator(index)
+{
+    return Boolean(precidence[operatorStack.at(index)])
+}
+ 
+ 
+function handleClosing()
+{
+    while(operatorStack.top() !== '(')
+    {
+        const operator = operatorStack.pop()
+ 
+        const operand2 = operandStack.pop()
+        const operand1 = operandStack.pop()
+ 
+        operandStack.push(evaluate(operator,operand1,operand2))
+    }
+    operatorStack.pop()
+}
+ 
+function evaluate(operator,operand1,operand2)
+{
+    let result;
+    switch(operator)
+    {
+        case '+':
+            result = operand1 + operand2
+            break;
+        case '-':
+            result = operand1 - operand2
+            break;
+        case '*':
+            result = operand1 * operand2
+            break;
+        case '/':
+            result = (operand1 / operand2).toFixed(3)
+            break;
+        case '%':
+            result = (operand1 % operand2)
+            break;
+        case '^':
+            result = operand1 ** operand2
+            break;
+    }
+    return result
+}
+ 
+function handlePrecidence(currentOperator)
+{
+    //if top is operator
+    // console.log("Inside an Handle Precidnce : is Operator", isOperator(operatorStack.length-1))
+    if(isOperator(operatorStack.length-1))
+    {
+        // check for precidence
+        // console.log("operator Top : ",operatorStack.top())
+        if(precidence[currentOperator] < precidence[operatorStack.top()])
+        {
+            while(!operatorStack.isEmpty() && precidence[operatorStack.top()] > precidence[currentOperator])
+            {
+                // pop from the operator stack and then twice pop from operand stack
+ 
+                const operator = operatorStack.pop()
+                const operand2 = operandStack.pop()
+                const operand1 = operandStack.pop()
+ 
+                // eveluate it and push it
+                operandStack.push(evaluate(operator,operand1,operand2))
+ 
+            }
+        }
+    }
+    // console.log("going to push")
+    operatorStack.push(currentOperator)
+    // console.log(operatorStack)
+}
+ 
+function handleOperation(char)
+{
+    switch(char)
+    {
+        case '(':
+            operatorStack.push('(')
+            break;
+ 
+        case ')':
+                handleClosing()
+                break;
+        case '^':
+                operatorStack.push('^')
+                break;
+        case '*':
+        case '/':
+        case '%':
+                handlePrecidence(char)
+                break;
+        case '+':
+        case '-':
+                handlePrecidence(char)
+                break;
+        default:
+            //here operand come so just append to the result
+            operandStack.push(Number(char))
+            // prefixExpression += char
+    }
+}
+ 
+export function infixEvalution(expression)
+{
+    operatorStack = new Array()
+    operandStack = new Array()
+    operandStack.push(0)
+ 
+    let numberString = '';
+    // for(const char of expression)
+    console.log("called : ",expression)
+    for(let i=0;i<expression.length;i++)
+    {
+        console.log("i",i,expression[i]);
+       
+        console.log("condition:",!precidence[expression[i]])
+        if(!precidence[expression[i]])
+        {
+            numberString += expression[i];
+            if((i< expression.length && precidence[expression[i+1]]) || i === expression.length-1)
+            {
+                console.log(numberString)
+                handleOperation(numberString)
+                numberString = '';
+            }
+        }
+        else
+        {
+            console.log(expression[i])
+            handleOperation(expression[i])
+        }
+    }
+ 
+    //check if operatorStack is Empty
+    while(!operatorStack.isEmpty())
+    {
+        // console.log("operatorStack is EMpty:",operatorStack.isEmpty())
+        const operator = operatorStack.pop()
+ 
+        const operand2 = operandStack.pop()
+        const operand1 = operandStack.pop()
+ 
+        operandStack.push(evaluate(operator,operand1,operand2))
+    }
+ 
+    console.log(operandStack.top())
+    return operandStack.top();
+}
+ 
+// infixEvalution("5+3")
+// infixEvalution("5+3*2")
+// infixEvalution("((5*2+3)+(2-3+5))")
+// infixEvalution("-5")
+// infixEvalution("((5*2+3)+(2-3*5))")
+ 
+ 
+//* case to be handle
+//! -89+89 ?
+//^ handle . case (might be handled)
