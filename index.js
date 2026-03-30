9fe556ec2190ef0d190001a79d9890abbd511447
krish-patel-simform <krish.patel@simformsolutions.com>
2026-03-30 15:44:51 +0530

    feat:implement logic to handle fraction number

--- index.js
+++ index.js
@@ -1,137 +1,158 @@
-import {infixEvalution} from './stack.js'
- 
+import { infixEvalution } from './stack.js'
+
 // Dom Manipulations
 const keyPad = document.querySelector('.calculator-operation__buttons')
- 
+
 const screen = document.getElementById('screen')
 console.log(screen)
- 
- 
+
+
+class Calculator {
+
+    getNumberForOperation() {
+        let ind = screen.value.length - 1;
+        let value = '';
+        console.log("called")
+        // until we did not get non digit
+        while (ind >= 0 && !Object.is(Number(screen.value.at(ind)), NaN)) {
+            console.log("before", screen.value)
+            value = screen.value.at(ind) + value;
+            screen.value = screen.value.slice(0, -1);
+            console.log("after", screen.value)
+            console.log("VAlue:", value)
+            ind--;
+        }
+        return value
+    }
+
+
+    equals() {
+        const result = infixEvalution(screen.value);
+        console.log("result:" + result);
+        console.log("type of result:",typeof result)
+        screen.value = Number.isNaN(result) ? "Error" : result;
+    }
+
+    clearScreen() {
+        screen.value = ""
+    }
+
+    eraseLast() {
+        screen.value = screen.value.slice(0, -1)
+    }
+
+    factorial(n) {
+        let fact = 1;
+        for (let i = 1; i <= n; i++) {
+            fact *= i
+        }
+        console.log("fact:s" + fact)
+        return fact;
+    }
+
+    sqrt() {
+        const result = this.getNumberForOperation();
+        screen.value += Math.sqrt(result)
+    }
+
+    square() {
+        const result = this.getNumberForOperation()
+        screen.value += Math.pow(result, 2);
+    }
+
+    log() {
+        const result = this.getNumberForOperation();
+        screen.value += Math.log10(result)
+    }
+
+    powerOf10() {
+        const result = this.getNumberForOperation();
+        screen.value += Math.pow(10, result);
+    }
+}
+
+const calculator = new Calculator()
+
 //* attack and onClick event
- 
-keyPad.addEventListener('click',(e)=>{
- 
-    const value = e.target.dataset.value;
-   
-    switch(value)
+
+keyPad.addEventListener('click', (e) => {
+
+    const button = e.target.closest('.btn')
+    if (!button) return;
+
+    handleEvent(button.dataset.value)
+
+    e.stopPropagation();
+})
+
+
+//* attach key event
+document.addEventListener('keydown',(e)=>{
+
+    console.log(e)
+
+
+    const regex = /^[\d+\-/*().]+$/;
+    console.log(regex.test(e.key))
+    if(regex.test(e.key))
     {
-        case "equals-":
-            equals();
+        console.log("regex is true")
+        handleEvent(e.key)
+    }
+    else if(e.key === 'Backspace')
+    {
+        handleEvent('erase')
+    }
+    else if(e.key === 'c' || e.key === 'C')
+    {
+        handleEvent('clear')
+    }
+    else if(e.key === 'Enter')
+    {
+        handleEvent('equals')
+    }
+})
+
+//^ utility function
+
+function handleEvent(value) {
+    // const value = button.dataset.value;
+
+    switch (value) {
+        case "equals":
+            calculator.equals();
             break;
         case "clear":
-            clearScreen()
-            break;  
+            calculator.clearScreen();
+            break;
         case "erase":
-            eraseLast();
+            calculator.eraseLast();
             break;
         case "factorial":
-            screen.value = screen.value.slice(0,-1) +  +factorial(screen.value.at(-1))
+            screen.value = screen.value.slice(0, -1) + +calculator.factorial(screen.value.at(-1))
             break;
         case 'sqrt':
-            sqrt();
+            calculator.sqrt();
             break;
         case 'log':
-            log();
+            calculator.log();
             break;
         case 'square':
-            square();
+            calculator.square();
             break;
         case 'powerOf10':
-            powerOf10();
+            calculator.powerOf10();
             break;
         default:
             screen.value += value;
     }
     console.log(value);
- 
-    // call the infix Evalurion
- 
-    e.stopPropagation();
-},true)
- 
-//^ utility function
- 
-function getNumberForOperation()
-{
-    let ind = screen.value.length - 1;
-    let value = '';
-    console.log("called")
-    // until we did not get non digit
-    while(ind >= 0 && Number(screen.value.at(ind))!== NaN)
-    {
-        // console.log("before",screen.value)
-        value = screen.value.at(-1) + value;
-        // console.log("after",screen.value)
-        screen.value = screen.value.slice(0,-1);
-        // console.log("VAlue:",value)
-        ind--;
-    }
-    return value
 }
- 
- 
-function equals()
-{
-    const result = infixEvalution(screen.value);
-    console.log("result:" + result)
-    screen.value = result;
-}
- 
-function clearScreen()
-{
-    screen.value = ""
-}
- 
-function eraseLast()
-{
-    screen.value = screen.value.slice(0,-1)
-}
- 
-function factorial(n)
-{
-    let fact = 1;
-    for(let i=1;i<=n;i++)
-    {
-        fact *= i
-    }
-    console.log("fact:s" + fact)
-    return fact;
-}
- 
-function sqrt()
-{
-    // let ind = screen.value.length - 1;
-    // let value = '';
-    // console.log("called")
-    // // until we did not get non digit
-    // while(ind >= 0 && Number(screen.value.at(ind)))
-    // {
-    //     // console.log("before",screen.value)
-    //     value = screen.value.at(-1) + value;
-    //     // console.log("after",screen.value)
-    //     screen.value = screen.value.slice(0,-1);
-    //     // console.log("VAlue:",value)
-    //     ind--;
-    // }
-    // find the value
-    const result = getNumberForOperation();
-    screen.value += Math.sqrt(result)
-}
- 
-function square()
-{
-    const result = getNumberForOperation()
-    screen.value += Math.pow(result,2);
-}
- 
-function log()
-{
-    const result = getNumberForOperation();
-    screen.value += Math.log10(result)
-}
- 
-function powerOf10()
-{
-    const result = getNumberForOperation();
-    screen.value += Math.pow(10,result);
-}
+
+
+//^ pending task ?
+// (1/2)
+//e
+//key event handle
+//.
+// mod and [x]
--- stack.js
+++ stack.js
@@ -1,52 +1,47 @@
-Array.prototype.isEmpty = function()
-{
+Array.prototype.isEmpty = function () {
     return this.length === 0;
 }
-Array.prototype.top = function()
-{
-    return this[this.length-1];
+Array.prototype.top = function () {
+    return this[this.length - 1];
 }
- 
+
 // const stack = []
 let operandStack;
-let operatorStack;  
- 
+let operatorStack;
+
 const precidence = {
-    '+' : 1,
-    '-' : 1,
-    '*' : 2,
-    '/' : 2,
-    '*' : 2,
-    '%' : 2,
-    '^' : 3,
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+    '*': 2,
+    '%': 2,
+    '^': 3,
+    '(': 0,
+    ')': 0
 }
- 
- 
-function isOperator(index)
-{
+
+
+function isOperator(index) {
     return Boolean(precidence[operatorStack.at(index)])
 }
- 
- 
-function handleClosing()
-{
-    while(operatorStack.top() !== '(')
-    {
+
+
+function handleClosing() {
+    while (operatorStack.top() !== '(') {
         const operator = operatorStack.pop()
- 
+
         const operand2 = operandStack.pop()
         const operand1 = operandStack.pop()
- 
-        operandStack.push(evaluate(operator,operand1,operand2))
+
+        operandStack.push(evaluate(operator, operand1, operand2))
     }
     operatorStack.pop()
 }
- 
-function evaluate(operator,operand1,operand2)
-{
+
+function evaluate(operator, operand1, operand2) {
     let result;
-    switch(operator)
-    {
+    switch (operator) {
         case '+':
             result = operand1 + operand2
             break;
@@ -57,7 +52,7 @@ function evaluate(operator,operand1,operand2)
             result = operand1 * operand2
             break;
         case '/':
-            result = (operand1 / operand2).toFixed(3)
+            result = Number((operand1 / operand2).toFixed(2))
             break;
         case '%':
             result = (operand1 % operand2)
@@ -68,28 +63,24 @@ function evaluate(operator,operand1,operand2)
     }
     return result
 }
- 
-function handlePrecidence(currentOperator)
-{
+
+function handlePrecidence(currentOperator) {
     //if top is operator
     // console.log("Inside an Handle Precidnce : is Operator", isOperator(operatorStack.length-1))
-    if(isOperator(operatorStack.length-1))
-    {
+    if (isOperator(operatorStack.length - 1)) {
         // check for precidence
         // console.log("operator Top : ",operatorStack.top())
-        if(precidence[currentOperator] < precidence[operatorStack.top()])
-        {
-            while(!operatorStack.isEmpty() && precidence[operatorStack.top()] > precidence[currentOperator])
-            {
+        if (precidence[currentOperator] < precidence[operatorStack.top()]) {
+            while (!operatorStack.isEmpty() && precidence[operatorStack.top()] > precidence[currentOperator]) {
                 // pop from the operator stack and then twice pop from operand stack
- 
+
                 const operator = operatorStack.pop()
                 const operand2 = operandStack.pop()
                 const operand1 = operandStack.pop()
- 
+
                 // eveluate it and push it
-                operandStack.push(evaluate(operator,operand1,operand2))
- 
+                operandStack.push(evaluate(operator, operand1, operand2))
+
             }
         }
     }
@@ -97,91 +88,105 @@ function handlePrecidence(currentOperator)
     operatorStack.push(currentOperator)
     // console.log(operatorStack)
 }
- 
-function handleOperation(char)
-{
-    switch(char)
-    {
+
+function handleOperation(char) {
+    switch (char) {
         case '(':
             operatorStack.push('(')
             break;
- 
         case ')':
-                handleClosing()
-                break;
+            handleClosing()
+            break;
         case '^':
-                operatorStack.push('^')
-                break;
+            operatorStack.push('^')
+            break;
         case '*':
         case '/':
         case '%':
-                handlePrecidence(char)
-                break;
+            handlePrecidence(char)
+            break;
         case '+':
         case '-':
-                handlePrecidence(char)
-                break;
+            handlePrecidence(char)
+            break;
         default:
             //here operand come so just append to the result
             operandStack.push(Number(char))
-            // prefixExpression += char
+        // prefixExpression += char
     }
 }
- 
-export function infixEvalution(expression)
-{
+
+export function infixEvalution(expression) {
     operatorStack = new Array()
     operandStack = new Array()
     operandStack.push(0)
- 
+
     let numberString = '';
     // for(const char of expression)
-    console.log("called : ",expression)
-    for(let i=0;i<expression.length;i++)
-    {
-        console.log("i",i,expression[i]);
-       
-        console.log("condition:",!precidence[expression[i]])
-        if(!precidence[expression[i]])
+    // console.log("called : ",expression)
+    for (let i = 0; i < expression.length; i++) {
+        // console.log("i",i,expression[i]);
+
+        // console.log("condition:",!precidence[expression[i]])
+        // 
+        // if (expression[i] === '(' || expression[i] === ')') {
+        //     handleOperation(expression[i])
+        // }
+        // else if (!precidence[expression[i]]) {
+        //     numberString += expression[i];
+        //     if ((i < expression.length && precidence[expression[i + 1]]) || i === expression.length - 1) {
+        //         // console.log(numberString)
+        //         handleOperation(numberString)
+        //         numberString = '';
+        //     }
+        // }
+        // else {
+        //     // console.log(expression[i])
+        //     handleOperation(expression[i])
+        // }
+
+        if(!Number.isNaN(Number(expression[i])) || expression[i] === '.')
         {
             numberString += expression[i];
-            if((i< expression.length && precidence[expression[i+1]]) || i === expression.length-1)
+            if((i< expression.length && Number.isNaN(Number(expression[i+1])) && expression[i+1] !== '.') || i === expression.length - 1)
             {
-                console.log(numberString)
+                console.log("before handle:",numberString)
                 handleOperation(numberString)
                 numberString = '';
             }
         }
         else
         {
-            console.log(expression[i])
             handleOperation(expression[i])
         }
     }
- 
+
     //check if operatorStack is Empty
-    while(!operatorStack.isEmpty())
-    {
+    // console.log("Stack is Empty:",operatorStack.isEmpty())
+    while (!operatorStack.isEmpty()) {
         // console.log("operatorStack is EMpty:",operatorStack.isEmpty())
         const operator = operatorStack.pop()
- 
+
         const operand2 = operandStack.pop()
         const operand1 = operandStack.pop()
- 
-        operandStack.push(evaluate(operator,operand1,operand2))
+
+        operandStack.push(evaluate(operator, operand1, operand2))
     }
- 
+
     console.log(operandStack.top())
     return operandStack.top();
 }
- 
+
 // infixEvalution("5+3")
 // infixEvalution("5+3*2")
 // infixEvalution("((5*2+3)+(2-3+5))")
 // infixEvalution("-5")
 // infixEvalution("((5*2+3)+(2-3*5))")
- 
- 
+
+// infixEvalution("(1/2)")
+// infixEvalution("2.5+2.5")
+
 //* case to be handle
 //! -89+89 ?
 //^ handle . case (might be handled)
+//! (1/2)
